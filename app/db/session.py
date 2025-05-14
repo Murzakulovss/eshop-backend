b99de272e5eeb6c91572047d73516da62738f9c6
Murzakulovss <samat3267@gmail.com>
2025-05-14 16:29:46 +0600

    refactor: declarative base replaced

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from app.core.settings import settings
 
 engine = create_engine(settings.database_url, echo=settings.debug)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 
 
