689715bc42b3b5b4460d478218b786897a7f3ca3
Murzakulovss <samat3267@gmail.com>
2025-05-15 13:57:53 +0600

    feat(crud): add get_user_by_id and get_users functions

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,11 +1,16 @@
 from sqlalchemy.orm import Session
-
 from app.core.security import hash_password
 from app.models.user import User
 from app.schemas.user import UserCreate
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email==email).first()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_users(db:Session, limit = 100):
+    return db.query(User).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
     db_user = User(email = user.email, hashed_password = hash_password(user.password), is_active = True)
