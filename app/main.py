b70464ebbc7c93d2243317287fefb9c523f100e7
Murzakulovss <samat3267@gmail.com>
2025-05-16 16:16:14 +0600

    chore: include routes to main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+from app.api import users
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(users.router, prefix="/users", tags=["users"])
