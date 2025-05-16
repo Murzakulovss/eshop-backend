add88eaf8c3fdae2e3ab9bc37f12c54b4822c0b7
Murzakulovss <samat3267@gmail.com>
2025-05-16 16:11:59 +0600

    feat(crud API): add all routes

---
+++ app/api/users.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, FastAPI, Depends
+from typing import List
+from sqlalchemy.orm import Session
+from app.crud.user import create_user as create_user_crud, get_user_by_id
+from app.crud.user import get_users as get_users_crud
+from app.crud.user import update_user as update_user_crud
+from app.crud.user import delete_user as delete_user_crud
+from app.db.session import get_db
+from app.schemas.user import UserCreate, UserRead, UserUpdate
+
+router = APIRouter()
+
+@router.post("/users/")
+async def create_user(user: UserCreate,db: Session = Depends(get_db)):
+    return create_user_crud(db = db, user=user)
+
+@router.get("/users/{user_id}", response_model=UserRead)
+async def read_user(user_id:int, db: Session = Depends(get_db)):
+    return get_user_by_id(db=db, user_id=user_id)
+
+@router.get("/users/", response_model=List[UserRead])
+async  def get_users(limit: int = 100,db: Session = Depends(get_db)):
+    return get_users_crud(db=db, limit = limit)
+
+@router.put("/users/{user_id}", response_model= UserRead)
+async def update_user(user_update: UserUpdate,user_id:int, db: Session = Depends(get_db)):
+    return update_user_crud(db = db, user_id=user_id, user_email=str(user_update.email))
+
+@router.delete("/users/{user_id}", response_model= UserRead)
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    return delete_user_crud(db, user_id)
