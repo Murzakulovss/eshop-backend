c310c5fcd4ea8b0736f79ade4f707e79aea53aaf
Murzakulovss <samat3267@gmail.com>
2025-05-17 22:45:13 +0600

    feat: update to pydantic v2

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,11 +1,18 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    database_url: str
+    db_host: str
+    db_port: str
+    db_name: str
+    db_user: str
+    db_password: str
     secret_key: str
-    debug: bool = False
+    debug: bool = True
 
-    class Config:
-        env_file = ".env"
+    @property
+    def database_url(self) -> str:
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
