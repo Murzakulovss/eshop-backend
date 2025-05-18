71f1ab95c643f89f3d9ee58a307f9ee699523ff8
Murzakulovss <samat3267@gmail.com>
2025-05-18 20:31:29 +0600

    refactor: user routes structure

--- app/api/users.py
+++ app/api/users.py
@@ -15,12 +15,12 @@ from app.schemas.user import UserCreate, UserRead, UserUpdate
 router = APIRouter()
 
 
-@router.post("/users/", response_model=UserRead)
+@router.post("/", response_model=UserRead)
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user_crud(db=db, user=user)
 
 
-@router.get("/users/{user_id}", response_model=UserRead)
+@router.get("/{user_id}", response_model=UserRead)
 async def read_user(user_id: int, db: Session = Depends(get_db)):
     user = get_user_by_id(db=db, user_id=user_id)
     if not user:
@@ -28,12 +28,12 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 
-@router.get("/users/", response_model=List[UserRead])
+@router.get("/", response_model=List[UserRead])
 async def get_users(limit: int = 100, db: Session = Depends(get_db)):
     return get_users_crud(db=db, limit=limit)
 
 
-@router.put("/users/{user_id}", response_model=UserRead)
+@router.put("/{user_id}", response_model=UserRead)
 async def update_user(user_update: UserUpdate, user_id: int, db: Session = Depends(get_db)):
     updated_user = update_user_crud(db=db, user_id=user_id, user_email=str(user_update.email))
     if not updated_user:
@@ -41,7 +41,7 @@ async def update_user(user_update: UserUpdate, user_id: int, db: Session = Depen
     return updated_user
 
 
-@router.delete("/users/{user_id}", response_model=UserRead)
+@router.delete("/{user_id}", response_model=UserRead)
 async def delete_user(user_id: int, db: Session = Depends(get_db)):
     deleted_user = delete_user_crud(db, user_id)
     if not deleted_user:
