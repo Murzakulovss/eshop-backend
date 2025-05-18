3d58fe892b273ea30f44ad6502f4dc917f6ec232
Murzakulovss <samat3267@gmail.com>
2025-05-18 20:28:28 +0600

    chore: adjust tab spacing

--- app/models/user.py
+++ app/models/user.py
@@ -3,13 +3,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
 
-
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key = True)
     email: Mapped[str] = mapped_column(String(30))
     hashed_password: Mapped[str] = mapped_column(String(100))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-
-
 
