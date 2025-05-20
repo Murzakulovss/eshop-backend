6641f69d70a3d44afa8b604b0565586c87ba5826
Murzakulovss <samat3267@gmail.com>
2025-05-20 15:54:56 +0600

    feat: add connection with product

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+from app.models.product import Product
 
 class User(Base):
     __tablename__ = "users"
@@ -10,3 +11,4 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String(100))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
+    products: Mapped[list["Product"]] = relationship(back_populates="owner")
