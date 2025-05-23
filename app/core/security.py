c0c807c01f0bc6a494257b6dee2634805ce0c293
Murzakulovss <samat3267@gmail.com>
2025-05-23 14:20:01 +0600

    feat: add create_access_token method

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+
+SECRET_KEY = "your_secret_key_should_be_long_and_random"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,4 +13,15 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode["exp"] = expire
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 
