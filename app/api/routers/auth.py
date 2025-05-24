d7d55a2fd5def5011af7c3305e050828e7b2f400
Murzakulovss <samat3267@gmail.com>
2025-05-24 21:43:22 +0600

    feat: add current_user method

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from app.core.security import verify_password, create_access_token
+from app.core.security import verify_password, create_access_token, SECRET_KEY
 from sqlalchemy.orm import Session
 from app.crud.user import User, get_user_by_email
 from app.db.session import get_db
-
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
 router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer("login")
 execution_401 = HTTPException(status_code=401, detail="Invalid email or password")
 
 @router.post("/login")
@@ -20,3 +22,13 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
 
     access_token = create_access_token(data = {"sub":user.email})
     return {"access_token": access_token,"token_type": "bearer"}
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session=Depends(get_db)):
+    payload = jwt.decode(token=token, key= SECRET_KEY, algorithms= "HS256")
+    email = payload["sub"]
+    if not email:
+        raise execution_401
+    user = get_user_by_email(db=db, email=email)
+    if not user:
+        raise execution_401
+    return user
