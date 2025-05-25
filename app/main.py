41d29903d27620959b15675cd6925cb902e6761b
Murzakulovss <samat3267@gmail.com>
2025-05-25 23:30:40 +0600

    feat: add custom_openapi method for auth through JWT

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+from itertools import product
 from fastapi import FastAPI
-from app.api.routers import users
-from app.api.routers import auth
+from app.api.routers import users,auth,product
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI(debug=True)
 
@@ -9,4 +10,30 @@ async def root():
     return {"message": "Hello World"}
 
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(product.router, prefix="/products", tags=["products"])
 app.include_router(auth.router)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="eshop API",
+        version="1.0.0",
+        description="Документация API с авторизацией через JWT",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+    for path in openapi_schema["paths"]:
+        for method in openapi_schema["paths"][path]:
+            openapi_schema["paths"][path][method]["security"] = [{"BearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
