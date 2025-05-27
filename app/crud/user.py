916e686eea14096779d700364a7d7bf82ca882d7
Murzakulovss <samat3267@gmail.com>
2025-05-27 16:22:08 +0600

    feat: add validation for create user & change return to raise

--- app/crud/user.py
+++ app/crud/user.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import Session
 from app.core.security import hash_password
 from app.models.user import User
 from app.schemas.user import UserCreate
+from fastapi import HTTPException, status
+
+exception404 = HTTPException(status_code=404, detail="User not found")
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email==email).first()
@@ -13,7 +16,16 @@ def get_users(db:Session,skip: int = 0, limit = 100):
     return db.query(User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(email = user.email, hashed_password = hash_password(user.password), is_active = True)
+    existing_user = get_user_by_email(db, str(user.email))
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already has taken"
+        )
+
+    db_user = User(email = user.email,
+                   hashed_password = hash_password(user.password),
+                   is_active = True)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -22,7 +34,7 @@ def create_user(db: Session, user: UserCreate):
 def update_user(db:Session, user_email: str, user_id:int):
     user = get_user_by_id(db, user_id)
     if not user:
-        return None
+        raise exception404
     user.email = user_email
     db.commit()
     db.refresh(user)
@@ -31,7 +43,7 @@ def update_user(db:Session, user_email: str, user_id:int):
 def delete_user(db:Session, user_id:int):
     user = get_user_by_id(db,user_id)
     if not user:
-        return None
+        raise exception404
     db.delete(user)
     db.commit()
     return user
