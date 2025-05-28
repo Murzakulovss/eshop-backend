378dab1a9610d9fcbfa810f076f06cc834f53515
Murzakulovss <samat3267@gmail.com>
2025-05-28 16:21:03 +0600

    feat: add fixture create user and auth header

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 from starlette.testclient import TestClient
 from app.main import app
+from tests.test_users import create_user
+
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture()
+def test_user(client):
+    return create_user(client)
+
+@pytest.fixture
+def auth_header(test_user, client):
+    response = client.post("/login", data={"username": test_user["email"], "password": "string"})
+    token = response.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
