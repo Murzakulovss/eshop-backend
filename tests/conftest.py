d7a6397e5c191b60b601cd53626281f448953ec2
Murzakulovss <samat3267@gmail.com>
2025-05-29 10:14:03 +0600

    feat: add clean db method

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
 import pytest
+from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
+
+from app.db import SessionLocal
 from app.main import app
+from app.models import Product, User
 from tests.test_users import create_user
 
+@pytest.fixture(autouse=True)
+def clean_db():
+    db: Session = SessionLocal()
+    db.query(Product).delete()
+    db.query(User).delete()
+    db.commit()
+    db.close()
 
 @pytest.fixture
 def client():
