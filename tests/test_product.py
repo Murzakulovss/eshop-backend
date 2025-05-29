960773aadf29462a864fe8c5a59adbf582a3c56c
Murzakulovss <samat3267@gmail.com>
2025-05-29 13:50:04 +0600

    refactor: add method create_product to reuse it

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,31 +1,33 @@
 from http.client import responses
 from app.models import Product
 
+def create_product_test(client, auth_header, name, description, price):
+    return client.post("/products/", headers = auth_header, json={
+        "name": name,
+        "description": description,
+        "price": price
+    })
+
 def test_create_product(client, auth_header):
     response = client.post("/products/", headers=auth_header, json={
-        "name": "iPhone",
-        "description": "Smartphone",
+        "name": "-",
+        "description": "-",
         "price": 999
     })
     assert response.status_code == 200
-    assert response.json()["name"] == "iPhone"
+    assert response.json()["name"] == "-"
 
 def test_read_product_by_id(client, auth_header):
-    create_response = client.post("/products/", headers=auth_header, json={
-        "name": "Test Read",
-        "description": "Read Desc",
-        "price": 500
-    })
-    assert create_response.status_code == 200
-    product = create_response.json()
+    response = create_product_test(client, auth_header, name="test read", description="read desc", price=100)
+    assert response.status_code == 200
+    product = response.json()
     product_id = product["id"]
 
     response = client.get(f"/products/{product_id}", headers=auth_header)
     assert response.status_code == 200
     data = response.json()
-    assert data["name"] == "Test Read"
+    assert data["name"] == "test read"
     assert data["id"] == product_id
-
 
 def test_read_products(client, auth_header):
     response = client.get("/products/", headers=auth_header)
@@ -33,12 +35,8 @@ def test_read_products(client, auth_header):
     assert isinstance(response.json(), list)
 
 def test_update_product(client, auth_header):
-    create_response = client.post("/products/", headers=auth_header, json={
-        "name": "Old Name",
-        "description": "Old Description",
-        "price": 500
-    })
-    product_id = create_response.json()["id"]
+    response = create_product_test(client, auth_header, name="old name", description="old desc", price=100)
+    product_id = response.json()["id"]
 
     update_response = client.put(f"/products/{product_id}", headers=auth_header, json={
         "name": "New Name",
@@ -52,12 +50,8 @@ def test_update_product(client, auth_header):
     assert updated["price"] == 700
 
 def test_delete_product(client, auth_header):
-    create_response = client.post("/products/", headers=auth_header, json={
-        "name": "To Be Deleted",
-        "description": "Temporary",
-        "price": 100
-    })
-    product_id = create_response.json()["id"]
+    response = create_product_test(client, auth_header, name="delete", description="temporary", price=100)
+    product_id = response.json()["id"]
 
     delete_response = client.delete(f"/products/{product_id}", headers=auth_header)
     assert delete_response.status_code == 200
